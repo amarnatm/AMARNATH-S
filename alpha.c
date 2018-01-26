9a235016f3e793367e0a108d00b407cd058c510b
amarnatm <35653010+amarnatm@users.noreply.github.com>
2018-01-26 06:09:16 +0000

    Update and rename alpha to alpha.c

---
+++ alpha.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+void main()
+{
+  int year;
+scanf("%d",&year);
+printf("ENTER THE YEAR:");
+if(year%4==0)
+printf("The year is leap year");
+else
+printf("The year is not leap year");
+}

9f807fc24864f30f37b9ec1a52016a3b04b30f84
amarnatm <35653010+amarnatm@users.noreply.github.com>
2018-02-06 16:10:37 +0530

    Update am.c

--- am.c
+++ am.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
-#include<conio.h>
-int main()
+void main()
 {
  long long n;
     int count = 0;
@@ -8,5 +7,5 @@ int main()
     scanf("%lld", &n);
     while(n != 0)
     printf("Number of digits: %d", count);
-    getch();
+ 
 }

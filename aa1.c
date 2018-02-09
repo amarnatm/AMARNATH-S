44d6db0519a15a14d8c68d55baa742e91f649116
amarnatm <35653010+amarnatm@users.noreply.github.com>
2018-02-09 16:04:03 +0530

    Rename aaa.c to aa1.c

---
+++ aa1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int i,n,flag=0;
+    scanf("%d",&n);
+    for(i=2;i<n/2;i++)
+ {
+   if(n%i==0)
+ flag=1;
+}
+if(flag==0)
+printf("prime  number");
+else
+printf("not prime number");
+}

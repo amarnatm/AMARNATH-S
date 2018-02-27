2173fd188feb757317523841c90f7fdcc97737c0
amarnatm <35653010+amarnatm@users.noreply.github.com>
2018-02-27 16:15:46 +0530

    Create aba.c

---
+++ aba.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int i,f,n;
+printf("Enter the number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+f=n*i;
+printf("the first five multiples of five is %d",f);
+}
+return 0;
+}
